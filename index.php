129f414e91c7fa4fed19bebc07fd41ea20868946
shinyy <252865778@qq.com>
2016-09-13 15:22:20 +0800

    原型

---
+++ index.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta name="keywords" content="关键词1,关键词2,关键词3" />
+<meta name="description" content="对网站的描述" />
+<title>标题</title>
+<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+<link type="text/css" rel="stylesheet" href="css/jquery.dataTables.css"/>
+<link type="text/css" rel="stylesheet" href="css/style.css"/>
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" charset="utf8" src="http://cdn.datatables.net/1.10.12/js/jquery.dataTables.js"></script>
+<script type="text/javascript" src="js/js.js"></script>
+</head>
+<body>
+<table class="display example" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th></th>
+                <th>酒店ID</th>
+                <th>酒店名称</th>
+                <th>Office</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+ 
+        <tfoot>
+            <tr>
+                <th></th>
+                <th>酒店ID</th>
+                <th>酒店名称</th>
+                <th>Office</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+    </table>
+
+</body>
+</html>
---
+++ js/js.js
@@ -0,0 +1,134 @@
+
+	
+/* Formatting function for row details - modify as you need */
+function format ( d ) {
+    // `d` is the original data object for the row
+	
+    /*return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+        '<tr>'+
+            '<td>Full name:</td>'+
+            '<td>'+d.name+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>Extension number:</td>'+
+            '<td>'+d.extn+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>Extra info:</td>'+
+            '<td>And any further details here (images etc)...</td>'+
+        '</tr>'+
+    '</table>';*/
+	
+	return '<table class="display example" cellspacing="0" width="100%" border="1" borderColor="#dddddd">'+
+        '<tbody><tr role="row" class="markTr"><td class="details-control sp"></td><td colspan="4">大床房</td></tr><tr class="hideTr">'+
+            
+            '<td>9月13日</td><td>1</td><td>1</td><td>1</td><td>1</td>'+
+        '</tr><tr class="hideTr"><td>9月14日</td><td>2</td><td>2</td><td>2</td><td>2</td></tr>'+
+		
+		'<tr role="row"  class="markTr"><td class="details-control sp"></td><td colspan="4">小床房</td></tr><tr class="hideTr">'+
+            
+            '<td>9月13日</td><td>1</td><td>1</td><td>1</td><td>1</td>'+
+        '</tr><tr class="hideTr"><td>9月14日</td><td>2</td><td>2</td><td>2</td><td>2</td></tr>'+
+       
+    '</tbody></table>';
+}
+$(document).ready(function() {
+    var table = $('.example').DataTable( {
+        "ajax": "../ajax/data/objects.txt",
+        "columns": [
+            {
+                "class":          'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
+            },
+            { "data": "name" },
+            { "data": "position" },
+            { "data": "office" },
+            { "data": "salary" }
+        ],
+        "order": [[1, 'asc']]
+    } );
+    // Add event listener for opening and closing details
+    /*$('.example>tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
+    } );*/
+	
+	$('.example>tbody').on('click', 'td.details-control', function () {
+		//alert($(this).next().text())
+		if($(this).hasClass('sp')){
+			$(this).parent("tr").toggleClass("shown");
+			$(this).parent("tr").nextUntil(".markTr","tr").toggle();
+			}else{
+				    var tr = $(this).closest('tr');
+					var row = table.row( tr );
+					if ( row.child.isShown() ) {
+						// This row is already open - close it
+						row.child.hide();
+						tr.removeClass('shown');
+						
+					}
+					else {
+						// Open this row
+						row.child( format(row.data()) ).show();
+						tr.addClass('shown');
+						/*$(this).parent("tr").nextUntil(".markTr","tr").show();*/
+					}
+				}
+       
+    } );
+	
+	$('.example').on('click','td',function(){
+		
+		if($(this).parent('tr').hasClass('odd')||$(this).parent('tr').hasClass('even')||$(this).parent('tr').hasClass('markTr')){
+			$(this).siblings('.details-control').click();
+			}
+		})
+	
+	/*$('.example>tbody').on('click', 'td.details-control',function () {
+		
+		
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+		
+		function clickNum(){
+		this.num=0;
+		this.clickF=function(){
+			if(this.num==0){
+				row.child( format(row.data()) ).show();
+           		tr.addClass('shown');
+				}else{
+					 row.child.hide();
+            		tr.removeClass('shown');
+					}
+			}
+		}
+		var newClick=new clickNum();
+		
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            
+			
+			newClick.num=1;
+			newClick.clickF();
+        }
+        else {
+            // Open this row
+			newClick.num=0;
+			newClick.clickF();
+        }
+		
+		
+    } );*/
+} );
